3da064d9bb632466449b85914c3506969329c173
Liam O'Connell <liamslost@googlemail.com>
2025-02-08 12:22:23 +0000

    face animates albeit badly. adding routes rather than A tags. updated SVG for better grouping.

---
+++ src/Components/Home/index.jsx
@@ -0,0 +1,17 @@
+
+
+
+function Home() {
+    
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>
+        Welcome to my website! Here you can find information about me, my projects, and how to contact me.
+      </p>
+      </div>
+  );
+}
+
+
+export default Home;
---
+++ src/Components/Projects/index.jsx
@@ -0,0 +1,17 @@
+function Projects() {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <ul>
+        <li>
+    Project 1
+        </li>
+        <li>
+           Project 2
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Projects;
