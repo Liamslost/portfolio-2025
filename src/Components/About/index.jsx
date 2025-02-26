6700f132adfe68330bbcb8b0cecc32de36b2fdba
Liam O'Connell <liamslost@googlemail.com>
2025-02-26 16:49:49 +0000

    contact page created with avatar.

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -1,10 +1,20 @@
 function About() {
-  return( 
-    <div className="h-screen">
-      <h1>If you're passionate about building creative solutions, lets collaborate and mae something awesome</h1>
-
+  return (
+    <div className="min-h-[calc(100vh-11rem)] max-w-[300px] md:max-w-[760px] lg:max-w-[1000px] xl:max-w-[1200px] m-auto grid md:grid-cols-11 grid-cols-1 gap-5 pt-10 md:pt-25 pb-20">
+      <div className="size-full md:h-90 lg:h-90 contain-content rounded-2xl lg:col-span-3 md:col-span-4 border-2 border-tertiary mx-auto">
+        <img className="saturate-10 scale-150 translate-y-[15%] md:translate-y-0 md:scale-120" src="public/profie-pic-3.jpg" alt="tall dark handsome man" />
+      </div>
+      <div className="bg-secondary md:col-span-7 lg:col-span-8 py-4 px-10 space-y-4 text-sm text-text-primary rounded-2xl drop-shadow-[0_0_15px_rgba(255,255,255,0.1)]" >
+        <h3 className="opacity-50">liamslost/README.md</h3>
+        <h1 className="text-5xl font-bold">Hi.</h1>
+        <p className="opacity-50">I&apos;m Liam, a full-stack developer and designer with a focus on React, TypeScript, and data-driven applications. I love solving problems through clean, efficient code and creating projects that balance performance, usability, and creativity.</p>
+        <p className="opacity-50">My work spans from interactive visualizations (like a tool offset visualizer for machine operators) to web tools (like Karen Ipsum) and real-world applications that make complex data easier to understand. I take a practical, hands-on approach to development—iterating fast, learning from every project, and keeping things simple but effective.</p>
+        <p className="opacity-50">I&apos;m currently freelancing, working on a mix of client projects and personal builds while sharpening my skills in data visualization, backend performance, and scalable architectures.</p>
+        <p className="opacity-50">
+        <i>When I&apos;m not coding, I&apos;m probably at the gym, planning my next adventure, or making slow but steady progress on learning Japanese.</i></p>
+      </div>
     </div>
-);
+  );
 }
 
 export default About;
--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -11,12 +11,13 @@ function Navbar() {
   return (
     <>
       <div className={`${isHome ? "hidden" : "flex pt-10"}`}>
-        <img src="public/mini-avatar.png" alt="avatar" className="size-20 ml-15 "/>
-      <h1 className="text-text-primary text-5xl p-4 font-semibold">
+        <div className="size-20 bg-secondary rounded-full border-2 absolute z-0 ml-15"></div>
+        <img src="/mini-avatar.png" alt="avatar" className={`${isContact ? "opacity-0" : ""} size-20 ml-15 z-1`} />
+      <h1 className="text-text-primary text-2xl md:text-5xl p-4 font-semibold">
           Hi. I&apos;m Liam
         </h1>
       </div>
-      <div className={`${isHome ? "" : "float-right mr-30 absolute right-0 top-19"} `}>
+      <div className={`${isHome ? "" : "float-right mr-30 absolute right-0 top-19 "} `}>
         <Link
           to="/"
           className={`${
@@ -28,7 +29,7 @@ function Navbar() {
         <Link
           to="/about"
           className={`${
-            isHome ? " p-4 m-2 my-26 absolute bottom-0 rotate-270 " : " p-6 transition-[letter-spacing] duration-500"
+            isHome ? " p-4 m-2 my-26 absolute bottom-0 rotate-270 font-semibold " : " p-6 transition-[letter-spacing] duration-500"
           } ${ isAbout ? " tracking-[0.5rem] uppercase font-semibold transition-[letter-spacing] duration-500" : ""} h-fit text-2xl text-text-primary `}
         >
           About
@@ -36,7 +37,7 @@ function Navbar() {
         <Link
           to="/contact"
           className={`${
-            isHome ? " p-6 my-12 absolute rotate-90 right-0 " : " p-6 transition-[letter-spacing] duration-500 "
+            isHome ? " p-6 my-12 absolute rotate-90 right-0 font-semibold " : " p-6 transition-[letter-spacing] duration-500 "
           }  ${ isContact ? " tracking-[0.5rem] uppercase font-semibold " : ""} h-fit w-fit text-2xl text-text-primary `}
         >
           Contact
@@ -44,7 +45,7 @@ function Navbar() {
         <Link
           to="/projects"
           className={`${
-            isHome ? " p-4 my-18 absolute bottom-0 right-12 " : " p-6 transition-[letter-spacing] duration-500 "
+            isHome ? " p-4 my-18 absolute bottom-0 right-12 font-semibold " : " p-6 transition-[letter-spacing] duration-500 "
           } ${ isProjects ? " tracking-[0.5rem] uppercase font-semibold " : ""} h-fit w-fit text-2xl text-text-primary `} 
         >
           Projects
